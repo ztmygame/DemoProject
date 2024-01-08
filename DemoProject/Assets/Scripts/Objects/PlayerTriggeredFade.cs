99f5077fcf08590660c34cf59d19665a625ace80
ACBGZM <acbgzm@126.com>
2024-01-08 19:27:20 +0800

    TSK-21: import dotween & add player triggered fade object prefab

---
+++ DemoProject/Assets/Scripts/Objects/PlayerTriggeredFade.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class PlayerTriggeredFade : MonoBehaviour
+{
+    private SpriteRenderer[] m_sprite_renderers;
+
+    private void Awake()
+    {
+        m_sprite_renderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    public void FadeOut()
+    {
+        Color target_color = new Color(1, 1, 1, Configurations.m_target_alpha);
+        foreach(SpriteRenderer renderer in m_sprite_renderers)
+        {
+            renderer.DOColor(target_color, Configurations.m_fade_duration);
+        }
+    }
+
+    public void FadeIn()
+    {
+        Color target_color = new Color(1, 1, 1, 1);
+        foreach (SpriteRenderer renderer in m_sprite_renderers)
+        {
+            renderer.DOColor(target_color, Configurations.m_fade_duration);
+        }
+    }
+}
